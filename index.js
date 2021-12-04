5736da368a3961b072729c8e399c5874adcffb38
LuziaFernandes <87724388+LuziaFernandes@users.noreply.github.com>
2021-12-04 21:23:43 +0000

    Done the activities Page: HTML, CSS. Create a function so each button is displayed on click (JS).

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function showSensationsVisual() {
+   document.getElementById("sensations-description-visual").style.display = 'block';
+   }
+
+function showSensationsSmell() {
+    document.getElementById("sensations-description-smell").style.display = 'block';
+    }
+    
+function showSensationsPalate() {
+        document.getElementById("sensations-description-palate").style.display = 'block';
+        }
+
+function showSensationsTouch() {
+            document.getElementById("sensations-description-touch").style.display = 'block';
+            }
+function showSensationsListen() {
+                document.getElementById("sensations-description-listen").style.display = 'block';
+                }
