6ed5a7af2e84cdd7e810a92ed0c101e50715b455
LuziaFernandes <87724388+LuziaFernandes@users.noreply.github.com>
2021-12-05 21:09:18 +0000

    work on the buttons-sensations  (still to finish)

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
 
+
+
+
 function showSensationsVisual() {
-   document.getElementById("sensations-description-visual").style.display = 'block';
-   }
+    document.getElementById("sensations-description-visual").style.display = 'block';
+    document.getElementById("button-sensations-description").innerHTML = document.getElementById("button-sensations-description").innerHTML.replace('+', '-');
+
+    }
+
+function hideSensationsVisual() {
+    document.getElementById("sensations-description-visual").style.display = 'none';
+    document.getElementById("button-sensations-description").innerHTML = document.getElementById("button-sensations-description").innerHTML.replace('-', '+');
+
+}
+
+
+
 
 function showSensationsSmell() {
     document.getElementById("sensations-description-smell").style.display = 'block';
     }
+
+function hideSensationsSmell() {
+        document.getElementById("sensations-description-smell").style.display = 'none';
+    }
+    
     
 function showSensationsPalate() {
         document.getElementById("sensations-description-palate").style.display = 'block';
         }
-
+ function hideSensationsPalate() {
+            document.getElementById("sensations-description-palate").style.display = 'none';
+        }
+        
 function showSensationsTouch() {
             document.getElementById("sensations-description-touch").style.display = 'block';
             }
+function hideSensationsTouch() {
+                document.getElementById("sensations-description-touch").style.display = 'none';
+            }
+            
 function showSensationsListen() {
                 document.getElementById("sensations-description-listen").style.display = 'block';
+                }
+function hideSensationsListen() {
+                    document.getElementById("sensations-description-listen").style.display = 'none';
                 }
